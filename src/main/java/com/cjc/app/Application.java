8ca760c11d88a6c5230a3b25c7174672bd18271a
aaba95 <harshalsinh2025@gmail.com>
2025-09-07 23:52:48 +0530

    m1 method added

--- src/main/java/com/cjc/app/Application.java
+++ src/main/java/com/cjc/app/Application.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 //int x=100;
+
+       public void m1(){
+System.out.println("m1 method...");
+}     
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
